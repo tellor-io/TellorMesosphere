a1e6416602a1ba78eaafc94bbe78037f1eaaf061
Tally Wiesenberg <t.j.wies@gmail.com>
2021-07-12 09:18:19 -0400

    polygon depl

--- scripts/deploy-polygon.js
+++ scripts/deploy-polygon.js
@@ -1,21 +1,24 @@
 async function main() {
 
     let quorum = 2
-    let
+    let timeLimit = 60 //1 minute
+    let maximumDeviation = 2000 //20%
 
     // We get the contract to deploy
-    const MesosphereDepl = await ethers.getContractFactory("Mesosphere");
+    const MesosphereDepl = await ethers.getContractFactory("TellorMesosphere");
     const mesosphere = await MesosphereDepl.deploy(
-        "Hello, Hardhat!"
+      quorum,
+      timeLimit,
+      maximumDeviation
     );
   
     console.log("Mesosphere deployed to:", mesosphere.address);
-    console.log("quorum:")
+    console.log("quorum: ", quorum)
 
     //add reporters
-    mesosphere.addReporter("0x8c3bf3EB2639b2326fF937D041292dA2e79aDBbf")
-    mesosphere.addReporter("0x2cFC5bCE14862D46fBA3bb46A36A8b2d7E4aC040")
-    mesosphere.addReporter("0x6ffd0DFE81308794357F0F83339a4529e5564695")
+    await mesosphere.addReporter("0x8c3bf3EB2639b2326fF937D041292dA2e79aDBbf")
+    await mesosphere.addReporter("0x2cFC5bCE14862D46fBA3bb46A36A8b2d7E4aC040")
+    await mesosphere.addReporter("0x6ffd0DFE81308794357F0F83339a4529e5564695")
   }
   
   main()
